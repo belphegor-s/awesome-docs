1d438baa384ee82191738d62c85b28eb9a225e70
ayush sharma <ayush2162020@gmail.com>
2025-07-11 01:11:10 +0530

    style: sidebar solid color

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <div className="flex h-screen bg-gray-950 text-white relative">
         <div
           className={`
-            fixed top-0 left-0 z-50 h-full w-64 bg-slate-900-900 border-r border-slate-700
+            fixed top-0 left-0 z-50 h-full w-64 bg-gary-950 border-r border-slate-700
             transform transition-transform duration-300 ease-in-out
             md:static md:translate-x-0 md:z-auto
             ${drawerOpen ? 'translate-x-0' : '-translate-x-full'}
